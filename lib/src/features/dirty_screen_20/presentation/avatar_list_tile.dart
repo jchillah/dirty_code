f0755eca18a0f9418ee0e595d9630cce34afe0d1
jchillah <jchillah@gmail.com>
2024-05-21 13:42:22 +0200

    cleared screen 20

---
+++ lib/src/features/dirty_screen_20/presentation/avatar_list_tile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AvatarListTile extends StatelessWidget {
+  final String name;
+  final String position;
+  final NetworkImage networkImage;
+  const AvatarListTile(
+      {super.key,
+      required this.name,
+      required this.position,
+      required this.networkImage});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Badge(
+        label: Text(
+          '1',
+          style: TextStyle(color: Colors.white),
+        ),
+        child: CircleAvatar(
+          backgroundImage: networkImage,
+        ),
+      ),
+      title: Text(name),
+      subtitle: Text(position),
+      trailing: Icon(Icons.more_vert),
+    );
+  }
+}
--- lib/src/features/dirty_screen_20/presentation/dirty_screen_twenty.dart
+++ lib/src/features/dirty_screen_20/presentation/dirty_screen_twenty.dart
@@ -1,3 +1,4 @@
+import 'package:dirty_code/src/features/dirty_screen_20/presentation/avatar_list_tile.dart';
 import 'package:flutter/material.dart';
 
 class DirtyScreenTwenty extends StatelessWidget {
@@ -11,66 +12,30 @@ class DirtyScreenTwenty extends StatelessWidget {
       ),
       body: ListView(
         children: const [
-          ListTile(
-            leading: Badge(
-              label: Text(
-                '1',
-                style: TextStyle(color: Colors.white),
-              ),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://randomuser.me/api/portraits/men/11.jpg'),
-              ),
-            ),
-            title: Text('Michael Scott'),
-            subtitle: Text('Regional Manager'),
-            trailing: Icon(Icons.more_vert),
+          AvatarListTile(
+            name: "Michael Scott",
+            position: "Reginal Manager",
+            networkImage:
+                NetworkImage('https://randomuser.me/api/portraits/men/11.jpg'),
           ),
-          ListTile(
-            leading: Badge(
-              label: Text(
-                '4',
-                style: TextStyle(color: Colors.white),
-              ),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://randomuser.me/api/portraits/women/22.jpg'),
-              ),
-            ),
-            title: Text('Pam Beesly'),
-            subtitle: Text('Receptionist'),
-            trailing: Icon(Icons.more_vert),
+          AvatarListTile(
+            name: "Pam Beesly",
+            position: "Receptionist",
+            networkImage: NetworkImage(
+                'https://randomuser.me/api/portraits/women/22.jpg'),
           ),
-          ListTile(
-            leading: Badge(
-              label: Text(
-                '99+',
-                style: TextStyle(color: Colors.white),
-              ),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://randomuser.me/api/portraits/men/33.jpg'),
-              ),
-            ),
-            title: Text('Jim Halpert'),
-            subtitle: Text('Sales Representative'),
-            trailing: Icon(Icons.more_vert),
+          AvatarListTile(
+            name: 'Jim Halpert',
+            position: 'Sales Representative',
+            networkImage:
+                NetworkImage('https://randomuser.me/api/portraits/men/33.jpg'),
           ),
-          ListTile(
-            leading: Badge(
-              label: Text(
-                '2',
-                style: TextStyle(color: Colors.white),
-              ),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://randomuser.me/api/portraits/women/44.jpg'),
-              ),
-            ),
-            title: Text('Dwight Schrute'),
-            subtitle: Text('Assistant Regional Manager'),
-            trailing: Icon(Icons.more_vert),
-          ),
+          AvatarListTile(
+            name: 'Dwight Schrute',
+            position: 'Assistant Regional Manager',
+            networkImage: NetworkImage(
+                'https://randomuser.me/api/portraits/women/44.jpg'),
+          )
         ],
       ),
     );
